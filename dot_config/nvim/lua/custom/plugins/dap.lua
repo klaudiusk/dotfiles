605fa396e6066f6069b5c7f00bb97182c5efb52e
Klaudius Kalcher <klaudiusk@gmail.com>
2026-04-15 23:38:51 +0200

    ssh settings and nvim dap plugin

--- dot_config/nvim/lua/custom/plugins/dap.lua
+++ dot_config/nvim/lua/custom/plugins/dap.lua
@@ -1,7 +1,10 @@
 return {
   {
     'rcarriga/nvim-dap-ui',
-    dependencies = 'mfussenegger/nvim-dap',
+    dependencies = {
+      'mfussenegger/nvim-dap',
+      'nvim-neotest/nvim-nio',
+    },
     config = function()
       local dap = require('dap')
       local dapui = require('dapui')
@@ -9,10 +12,10 @@ return {
       dap.listeners.after.event_initialized['dapui_config'] = function()
         dapui.open()
       end
-      dap.listeners.after.event_terminated['dapui_config'] = function()
+      dap.listeners.before.event_terminated['dapui_config'] = function()
         dapui.close()
       end
-      dap.listeners.after.event_exited['dapui_config'] = function()
+      dap.listeners.before.event_exited['dapui_config'] = function()
         dapui.close()
       end
     end,
